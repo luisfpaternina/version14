2f7b0c445715a8dd8abdc085e38f6055a8e5a941
bitsluispaternina <53566346+luisfpaternina@users.noreply.github.com>
2022-04-22 08:56:47 -0500

    module

---
+++ account_payroll_ext/__manifest__.py
@@ -0,0 +1,29 @@
+{
+    'name': 'account payroll ext',
+
+    'version': '14.0.0.0',
+
+    'author': "ProcessControl",
+
+    'contributors': ['Luis Felipe Paternina'],
+
+    'website': "www.processcontrol.es",
+
+    'category': 'Account',
+
+    'depends': [
+
+        'account_accountant',
+        'hr',
+        'hr_attendance',
+    ],
+
+    'data': [
+    
+        #'security/ir.model.access.csv',
+        #'views/res_company.xml',
+               
+    ],
+    'installable': True
+}
+
