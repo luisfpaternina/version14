52d6fcf32aad0906f80bd44cd2690f41609f6e72
bitsluispaternina <53566346+luisfpaternina@users.noreply.github.com>
2022-04-22 10:58:06 -0500

    add

---
+++ pc_bim_custom_reports/__manifest__.py
@@ -0,0 +1,26 @@
+{
+    'name': 'pc BIM custom reports',
+
+    'version': '14.0.0.0',
+
+    'author': "ProcessControl",
+
+    'contributors': ['Luis Felipe Paternina'],
+
+    'website': "www.processcontrol.es",
+
+    'category': 'reports',
+
+    'depends': [
+
+        'base',
+        'bim_project',
+    ],
+
+    'data': [
+        
+        #'views/account_move_payroll.xml',
+               
+    ],
+    'installable': True
+}
